2619872d40ef46039e66f159616a14c3c2c494c2
Matthew Faltys <mfaltys@gmail.com>
2016-06-28 17:10:02 -0500

    New API with response model for added security

--- beacon.go
+++ beacon.go
@@ -2,32 +2,16 @@ package main
 
 import (
 	"crypto/rand"
-	"encoding/base64"
 	"fmt"
 	"github.com/gorilla/mux"
 	"golang.org/x/crypto/sha3"
 	"gopkg.in/gcfg.v1"
 	"gopkg.in/redis.v3"
-	"io"
 	"log"
 	"net/http"
-	"os"
 	"strings"
 	"time"
 )
-
-/*
-//================================================================
-// general strategy:
-// we take in a file, the filename is a hashed random string.
-// the file is stored with its filename as the hased string.
-// the random string (token) is returned back to the user.
-//
-// now when the user wants to retrive the file, he puts in the
-// token (random string from earlier). his request is hashed and
-// the stored hash is returned. ez
-//================================================================
-*/
 
 type Config struct {
 	Beacon struct {
@@ -67,8 +51,14 @@ func main() {
 
 	// all handlers. lookin funcy casue i have to pass redis handler
 	router := mux.NewRouter().StrictSlash(true)
-	router.HandleFunc("/token", func(w http.ResponseWriter, r *http.Request) {
-		token(w, r, client, "tmp")
+	router.HandleFunc("/provision", func(w http.ResponseWriter, r *http.Request) {
+		provision(w, r, client, "tmp")
+	}).Methods("POST")
+	router.HandleFunc("/update", func(w http.ResponseWriter, r *http.Request) {
+		update(w, r, client, "tmp")
+	}).Methods("POST")
+	router.HandleFunc("/remove", func(w http.ResponseWriter, r *http.Request) {
+		remove(w, r, client, "tmp")
 	}).Methods("POST")
 	router.HandleFunc("/{fdata}", func(w http.ResponseWriter, r *http.Request) {
 		handlerdynamic(w, r, client)
@@ -81,79 +71,112 @@ func handlerdynamic(w http.ResponseWriter, r *http.Request, client *redis.Client
 	fdata := vars["fdata"]
 
 	// hash the token that is passed
-	hash := sha3.Sum512([]byte(fdata))
-	hashstr := fmt.Sprintf("%x", hash)
+	clientIdHash := sha3.Sum512([]byte(fdata))
 
-	val, err := client.Get(hashstr).Result()
+	val, err := client.Get(fmt.Sprintf("ip:%x", clientIdHash)).Result()
 	if err != nil {
 		log.Printf("data does not exist")
+		w.WriteHeader(http.StatusNotFound)
 		fmt.Fprintf(w, "token not found")
 	} else {
 		//log.Printf("data exists")
-		ip := strings.Split(r.RemoteAddr, ":")[0]
-		log.Printf("Responsing to %s :: from: %s", fdata, ip)
-
-		decodeVal, _ := base64.StdEncoding.DecodeString(val)
-
-		file, _ := os.Create("tmpfile")
-		io.WriteString(file, string(decodeVal))
-		file.Close()
-
-		http.ServeFile(w, r, "tmpfile")
-		os.Remove("tmpfile")
+		fmt.Fprintf(w, "%s", val)
 	}
 }
 
-func token(w http.ResponseWriter, r *http.Request, client *redis.Client, state string) {
+func provision(w http.ResponseWriter, r *http.Request, client *redis.Client, state string) {
 	// get file POST from index
-	//fmt.Println("method:", r.Method)
 	r.ParseForm()
-	address := strings.TrimSpace(r.FormValue("address"))
+	clientId := strings.TrimSpace(r.FormValue("id"))
 
-	// generate token and hash to save
-	token := tokenGen(config.Beacon.TokenSize, client)
-	w.Header().Set("token", token)
-	fmt.Fprintf(w, "%s", token)
+	// sha3:512 hash the id
+	clientIdHash := sha3.Sum512([]byte(clientId))
 
-	// done with client, rest is server side
-	hash := sha3.Sum512([]byte(token))
-	hashstr := fmt.Sprintf("%x", hash)
-	fmt.Println("uploading:", token)
+	// check if the id exists (sec:<clientId>)
+	_, err := client.Get(fmt.Sprintf("sec:%x", clientIdHash)).Result()
+	if err != redis.Nil {
+		fmt.Println("DEBUG :: COLLISION")
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	} else {
+		// generate token, store hashed token in db
+		token := randStr(config.Beacon.TokenSize)
+		tokenHash := sha3.Sum512([]byte(token))
 
-	fileBase64Str := base64.StdEncoding.EncodeToString([]byte(address))
+		// return token to client
+		w.Header().Set("token", token)
+		fmt.Fprintf(w, "%s", token)
 
-	//println("uploading ", "file")
-	client.Set(hashstr, fileBase64Str, 0).Err()
-	if strings.EqualFold(state, "tmp") {
-		client.Expire(hashstr, (config.Beacon.TTL * time.Hour)).Err()
-		//fmt.Println("expire link generated")
+		// done with client, rest is server side
+		// sec:<hashed clientId> : hashed password
+		client.Set(fmt.Sprintf("sec:%x", clientIdHash), fmt.Sprintf("%x", tokenHash), 0).Err()
+
+		// if temp objects, set an expire link on them
+		if strings.EqualFold(state, "tmp") {
+			client.Expire(fmt.Sprintf("sec:%x", tokenHash), (config.Beacon.TTL * time.Hour)).Err()
+		}
 	}
 }
 
-func tokenGen(strSize int, client *redis.Client) string {
-	// generate new token
-	token := randStr(strSize)
-	// hash token
-	hash := sha3.Sum512([]byte(token))
-	hashstr := fmt.Sprintf("%x", hash)
+func update(w http.ResponseWriter, r *http.Request, client *redis.Client, state string) {
+	// get file POST from index
+	r.ParseForm()
+	clientId := strings.TrimSpace(r.FormValue("id"))
+	clientSec := strings.TrimSpace(r.FormValue("sec"))
+	clientAddress := strings.TrimSpace(r.FormValue("address"))
 
-	_, err := client.Get(hashstr).Result()
+	// sha3:512 hash the id and sec
+	clientIdHash := sha3.Sum512([]byte(clientId))
+	clientSecHash := sha3.Sum512([]byte(clientSec))
 
-	for err != redis.Nil {
-		fmt.Println("DEBUG :: COLLISION")
-		token = randStr(strSize)
-		hash := sha3.Sum512([]byte(token))
-		hashstr := fmt.Sprintf("%x", hash)
-
-		_, err = client.Get(hashstr).Result()
-		// do not ddos box if db is full
-		time.Sleep(time.Second * 1)
+	// check if id exists
+	storedSecHash, err := client.Get(fmt.Sprintf("sec:%x", clientIdHash)).Result()
+	if err != redis.Nil {
+		// id exists, make sure clientSecHash is the same as the stored version
+		if fmt.Sprintf("%x", clientSecHash) == storedSecHash {
+			// client is authed, update clientAddress
+			w.WriteHeader(http.StatusOK)
+			client.Set(fmt.Sprintf("ip:%x", clientIdHash), clientAddress, 0).Err()
+		} else {
+			// client auth failed
+			w.WriteHeader(http.StatusForbidden)
+		}
+	} else {
+		w.WriteHeader(http.StatusBadRequest)
+		// id does not exist
 	}
-	return token
+}
+
+func remove(w http.ResponseWriter, r *http.Request, client *redis.Client, state string) {
+	// get file POST from index
+	r.ParseForm()
+	clientId := strings.TrimSpace(r.FormValue("id"))
+	clientSec := strings.TrimSpace(r.FormValue("sec"))
+
+	// sha3:512 hash the id and sec
+	clientIdHash := sha3.Sum512([]byte(clientId))
+	clientSecHash := sha3.Sum512([]byte(clientSec))
+
+	// check if id exists
+	storedSecHash, err := client.Get(fmt.Sprintf("sec:%x", clientIdHash)).Result()
+	if err != redis.Nil {
+		// id exists, make sure clientSecHash is the same as the stored version
+		if fmt.Sprintf("%x", clientSecHash) == storedSecHash {
+			// client is authed
+			w.WriteHeader(http.StatusOK)
+			client.Del(fmt.Sprintf("ip:%x", clientIdHash))
+			client.Del(fmt.Sprintf("sec:%x", clientIdHash))
+		} else {
+			// client auth failed
+			w.WriteHeader(http.StatusForbidden)
+		}
+	} else {
+		w.WriteHeader(http.StatusBadRequest)
+		// id does not exist
+	}
 }
 
 func randStr(strSize int) string {
-	//dictionary := "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
 	dictionary := config.Beacon.TokenDictionary
 
 	var bytes = make([]byte, strSize)
