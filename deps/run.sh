5980df6b04aa7264ce9609fd776b82a8e5dfdbae
dlepertd3 <dlepert@d3banking.com>
2016-11-29 14:36:36 -0600

    pr to put some donk on it

--- deps/run.sh
+++ deps/run.sh
@@ -9,11 +9,15 @@ echo "save 30 1" >> /redis.conf
 redis-server /redis.conf
 
 echo -e ""
-echo -e "\e[36m     _     /   /    /    \e[39m"
-echo -e "\e[36m    |_|   /   /    /     \e[39m"
-echo -e "\e[36m   [|_|]<    |     |     \e[39m"
-echo -e "\e[36m    | |   \   \    \     \e[39m"
-echo -e "\e[36m    | |    \   \    \    \e[39m"
+echo -e "\e[36m  /'\    _  --  ¯¯    \e[39m"
+echo -e "\e[36m  |i|< ¯              \e[39m"
+echo -e "\e[36m '---' ¯ -  __        \e[39m"
+echo -e "\e[36m |\"  |          --    \e[39m"
+echo -e "\e[36m /   \                \e[39m"
+echo -e "\e[36m | \" |                \e[39m"
+echo -e "\e[36m |  \"|                \e[39m"
+echo -e "\e[36m |   |                \e[39m"
+echo -e "\e[36m/  [] \               \e[39m"
 echo -e "\e[0m-------- \e[31mbeacon\e[39m ---------\e[0m"
 echo -e "\e[0m:: \e[31m$VER_NO\e[39m ::\e[0m"
 
